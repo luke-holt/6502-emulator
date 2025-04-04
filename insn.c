120fcd88eaa9e1b0c4820070a702011d8ea928a9
Luke Holt <luke@lukeholt.dev>
2025-04-03 22:22:02 -0400

    style

--- insn.c
+++ insn.c
@@ -4,38 +4,12 @@
  
 #define UTIL_IMPL
 #include "util.h"
- 
-void print_mode_info(void) {
-    ulog(UNONE, "%-5s %-19s %-11s %s", "Abbr", "Mode", "Syntax", "Description");
-    for (int i = 0; i < ARRLEN(modeinfo); i++) {
-        ulog(UNONE, "%-5s %-19s %-11s %s", modeinfo[i][0], modeinfo[i][1], modeinfo[i][2], modeinfo[i][3]);
-    }
-}
 
-void print_insn_info(u8 insn) {
-    ulog(UNONE, "%s -> %s", insnnames[insn][0], insnnames[insn][1]);
-
-    // 3, 19, 11, 114
-    ulog(UNONE, "%-3s %-5s %-19s %-11s", "OPC", "Abbr", "Mode", "Syntax");
-
-    // iter through insn map
-    for (int opc = 0; opc < ARRLEN(insnmap); opc++) {
-        // insn matches name
-        if (insnmap[opc] == insn) {
-            u8 m = modemap[opc];
-            ulog(UNONE, "%02X  %-5s %-19s %-11s", opc, modeinfo[m][0], modeinfo[m][1], modeinfo[m][2]);
-        }
-    }
-}
-
-int insn_from_name(const char *name) {
-    for (int i = 0; i < ARRLEN(insnnames); i++) {
-        if (strcmp(name, insnnames[i][0]) == 0) {
-            return i;
-        }
-    }
-    return -1;
-}
+static void print_mode_title_row(void);
+static void print_mode_row(int i);
+static void print_mode_info(void);
+static void print_insn_info(u8 insn);
+static int insn_from_name(const char *name);
 
 int
 main(int argc, char *argv[])
@@ -73,4 +47,45 @@ main(int argc, char *argv[])
     print_insn_info(insn);
 
     return 0;
+}
+
+void
+print_mode_title_row(void) {
+    // 3, 19, 11, 5, 114
+    ulog(UNONE, "%-5s %-19s %-11s %-5s %s", "Abbr", "Mode", "Syntax", "Bytes", "Description");
+}
+
+void
+print_mode_row(int i) {
+    // 3, 19, 11, 5, 114
+    ulog(UNONE, "%-5s %-19s %-11s %-5s %s", modeinfo[i][0], modeinfo[i][1], modeinfo[i][2], modeinfo[i][3], modeinfo[i][4]);
+}
+
+void
+print_mode_info(void) {
+    print_mode_title_row();
+    for (int i = 0; i < ARRLEN(modeinfo); i++)
+        print_mode_row(i);
+}
+
+void
+print_insn_info(u8 insn) {
+    ulog(UNONE, "%s -> %s", insnnames[insn][0], insnnames[insn][1]);
+
+    print_mode_title_row();
+
+    // iter through insn map
+    for (int opc = 0; opc < ARRLEN(insnmap); opc++) {
+        // insn matches name
+        if (insnmap[opc] == insn)
+            print_mode_row(modemap[opc]);
+    }
+}
+
+int
+insn_from_name(const char *name) {
+    for (int i = 0; i < ARRLEN(insnnames); i++)
+        if (strcmp(name, insnnames[i][0]) == 0)
+            return i;
+    return -1;
 }
